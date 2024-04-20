0baae14fdd2f184c3e2d05e000b0b4dea08951ee
Gaurav-Verma07 <cosmosian07@gmail.com>
2024-04-20 18:51:04 +0530

    fix: input field chiping

--- src/components/DataInput/DataInput.tsx
+++ src/components/DataInput/DataInput.tsx
@@ -1,8 +1,9 @@
-import { Box, Button, Group } from '@mantine/core';
+import { Box, Button, Chip, Group } from '@mantine/core';
 import {
   IconBellRinging,
   IconFingerprint,
   IconKey,
+  IconPoint,
   IconReceipt2,
 } from '@tabler/icons-react';
 import { useState } from 'react';
@@ -17,49 +18,76 @@ import SkillsInfo from '@/components/SkillsInfo/SkillsInfo';
 import SummaryInfo from '@/components/SummaryInfo/SummaryInfo';
 
 const data = [
-  { index: 0, label: 'Personal Info', icon: IconBellRinging },
-  { index: 1, label: 'Summary', icon: IconBellRinging },
-  { index: 2, label: 'Education', icon: IconReceipt2 },
-  { index: 3, label: 'Experiences', icon: IconReceipt2 },
-  { index: 4, label: 'Project', icon: IconFingerprint },
-  { index: 5, label: 'Skills', icon: IconKey },
+  {
+    index: 0,
+    label: 'Personal Info',
+    icon: IconBellRinging,
+    component: <PersonalInfo />,
+  },
+  {
+    index: 1,
+    label: 'Summary',
+    icon: IconBellRinging,
+    component: <SummaryInfo />,
+  },
+  {
+    index: 2,
+    label: 'Education',
+    icon: IconReceipt2,
+    component: <EducationInfo />,
+  },
+  {
+    index: 3,
+    label: 'Experiences',
+    icon: IconReceipt2,
+    component: <ExperienceInfo />,
+  },
+  {
+    index: 4,
+    label: 'Project',
+    icon: IconFingerprint,
+    component: <ProjectsInfo />,
+  },
+  { index: 5, label: 'Skills', icon: IconKey, component: <SkillsInfo /> },
 ];
 
 const DataInput = () => {
-  const [active, setActive] = useState(0);
-
-  const links = data.map((item) => (
-    <a
-      className={classes.link}
-      data-active={item.index === active || undefined}
-      // href={item.link}
-      key={item.index}
-      onClick={(event) => {
-        event.preventDefault();
-        setActive(item.index);
-      }}
-    >
-      <item.icon className={classes.linkIcon} stroke={1.5} />
-      <span>{item.label}</span>
-    </a>
-  ));
+  const [active, setActive] = useState(data[0].label);
 
   return (
     <Box className={classes.container}>
       <div className={classes.fields}>
-        {links}
-        <Button style={{ width: '100%' }}>Submit</Button>
+        <Chip.Group multiple={false} value={active} onChange={setActive}>
+          <Group>
+            {data.map((item) => (
+              <Chip
+                icon={<IconPoint />}
+                className={classes.link}
+                // data-active={item.index === active || undefined}
+                // href={item.link}
+                value={item.label}
+                key={item.index}
+              >
+                {/* <item.icon className={classes.linkIcon} stroke={1.5} /> */}
+                <span>{item.label}</span>
+              </Chip>
+            ))}
+            <Button style={{}}>Submit</Button>
+          </Group>
+        </Chip.Group>
       </div>
       <Box w={600} pt={20} className={classes.mainBox}>
-        {active === 0 && <PersonalInfo />}
+        {/* {active === 0 && <PersonalInfo />}
         {active === 1 && <SummaryInfo />}
         {active === 2 && <EducationInfo />}
         {active === 3 && <ExperienceInfo />}
         {active === 4 && <ProjectsInfo />}
-        {active === 5 && <SkillsInfo />} {/* <RichEditor /> */}
-        <Group pt={15} grow>
-          <Button>Save</Button>
-        </Group>
+        {active === 5 && <SkillsInfo />} */}
+        {/* <RichEditor /> */}
+        {
+          data.filter((item) => item.label === active && item.component)[0]
+            .component
+        }
       </Box>
     </Box>
   );
